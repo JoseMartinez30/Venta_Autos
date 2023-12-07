d2e190cc29ae68996286981077636c18708a4892
Jose Martinez <martinezbravo2002@gmail.com>
2023-12-06 23:54:52 -0500

    agregando nuevas interfaces

---
+++ src/modelos/interfaces/MetodosVistasRegistrar.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelos.interfaces;
+
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author usuario
+ */
+public interface MetodosVistasRegistrar {
+
+    public void registrar(ActionEvent e);
+
+    public void Cancelar(ActionEvent e);
+
+}
